f4625f17de28b649d025e466829ace09a1a08f74
AniSanti18 <159614229+AniSanti18@users.noreply.github.com>
2024-12-11 15:31:28 +0700

    Add files via upload

---
+++ storage/framework/views/06e409e790954a059f6ce3547cd8f1f1adfff7f7.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+    
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+    background-color: #f83eef;
+    color: #333;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+h1 {
+    color: #bcf323;
+}
+
+a {
+    text-decoration: none;
+    color: rgb(26, 24, 24);
+    background-color: #0066cc;
+    padding: 10px 20px;
+    border-radius: 5px;
+}
+
+a:hover {
+    background-color: #1eda66;
+}
+
+    </style>
+</head>
+<body>
+    <h1>Selamat Datang!</h1>
+    <p><strong>NIM:</strong>2257401023</p>
+    <p><strong>Nama:</strong>Ani Santi</p>
+    <p><strong>Kelas:</strong>MI22B</p>
+    <a href="/logout">Logout</a>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\uts-laravel-2257401023\resources\views/home.blade.php ENDPATH**/ ?>
